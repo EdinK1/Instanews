1e1703db2c2a6b734842a9c325c13a0a558fea57
EdinK1 <edinkaymakchi@gmail.com>
2019-05-22 12:29:29 -0700

    fix a gulpfile bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 const gulp = require('gulp'),
-      terser = require("gulp-terser"),
-      rename = require("gulp-rename"),
+      terser = require('gulp-terser'),
+      sass = require('gulp-sass'),
+      rename = require('gulp-rename'),
       browserSync = require('browser-sync').create(),
       eslint = require('gulp-eslint');
 
+    //   sass.compiler = require('node-sass');
 
 
-gulp.task("scripts", function() {
+      gulp.task('sass', function () {
+        return gulp.src('app/scss/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('./build/css'));
+      });
+
+
+
+
+gulp.task('scripts', function() {
 return gulp
-  .src("app/js/*.js")
+  .src('app/js/*.js')
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError())
@@ -16,8 +27,8 @@ return gulp
       keep_fnames: false,
       toplevel: true
   }))
-  .pipe(rename({ extname: ".min.js" }))
-  .pipe(gulp.dest("./build/js"));
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('browser-sync', function() {
@@ -26,14 +37,16 @@ gulp.task('browser-sync', function() {
             baseDir: './'
         }
     });
+    gulp.watch(['app/scss/*.scss', 'app/js/*.js']).on('change', browserSync.reload);
 });
 
 gulp.task('reload', function() {
     browserSync.reload();
 });
 
-gulp.task("watch", function() {
-    gulp.watch(["js/*.js", "index.html"], gulp.series("scripts", 'reload'));
+gulp.task('watch', function() {
+    gulp.watch('app/scss/*.scss', gulp.series('sass'));
+    gulp.watch('app/js/*.js', gulp.series('scripts'));
   });
 
-gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
