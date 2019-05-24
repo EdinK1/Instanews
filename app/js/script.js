f76a65395a39d02d391736b4e1fc86ef9377217a
EdinK1 <edinkaymakchi@gmail.com>
2019-05-23 20:49:09 -0700

    fetch data from api

---
+++ app/js/script.js
@@ -0,0 +1,35 @@
+let output = $('.articles');
+let select = $('#categories');
+
+function getData () { 
+
+    output.text('');
+
+    const section = $('#categories option:selected').val();
+
+$.ajax({
+    method: 'GET',
+    url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=cMfA0NO0S102DbxZsVap17SSVyu0hWxT`
+  })
+  .done(function (data) {
+
+ 
+    for (let i = 0; i < data.results.length; i++) {
+
+        let img = data.results[i].multimedia[4].url;
+        let snippet = data.results[i].abstract;
+        let url = data.results[i].url;
+
+
+    let results =
+    '<li class="article">' +
+    '<a href="' + url + '" alt="url" target="_blank">' + '<img src="' + img + '">' + '</a>' +
+    '<p class="description">' + snippet + '</p>' +
+    '</li>';
+
+    output.append(results);
+    }
+  });
+}
+
+select.on('change', getData);
