e83f06c11c340a6b4820fe9afd080f0cfccc833a
EdinK1 <edinkaymakchi@gmail.com>
2019-05-26 12:16:41 -0700

    implement tablet and desktop design

--- app/js/script.js
+++ app/js/script.js
@@ -4,52 +4,75 @@ let header = $('.main-header');
 let logo = $('.logo');
 
 function loader() {
-    $('.preload').show();
-  }
-    
+  $('.preload').show();
+}
+
 function showResults() {
-    setTimeout(function() {
-      $('.preload').hide();      
-    }, 1000);
-    }
-  
-function getData () { 
+  setTimeout(function () {
+    $('.preload').hide();
+  }, 1000);
+}
+
+function getData() {
 
 
-    loader();
-    showResults();
+  loader();
+  showResults();
 
-    output.text('');
+  output.text('');
 
-    const section = $('#categories option:selected').val();
+  const section = $('#categories option:selected').val();
 
-$.ajax({
-    method: 'GET',
-    url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=cMfA0NO0S102DbxZsVap17SSVyu0hWxT`
-  })
-  .done(function (data) {
- 
-    for (let i = 0; i < data.results.length; i++) {
+  $.ajax({
+      method: 'GET',
+      url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=cMfA0NO0S102DbxZsVap17SSVyu0hWxT`
+    })
+    .done(function (data) {
+
+      for (let i = 0; i < data.results.length; i++) {
         let img = data.results[i].multimedia[4].url;
         let snippet = data.results[i].abstract;
         let url = data.results[i].url;
 
 
-    let results =
-    '<li class="article">' +
-    '<a href="' + url + '" alt="url" target="_blank">' + '<img src="' + img + '">' + '</a>' +
-    '<p class="description">' + snippet + '</p>' +
-    '</li>';
+        let results =
+          '<li class="article">' +
+          '<a href="' + url + '" alt="url" target="_blank">' + '<img src="' + img + '">' + '</a>' +
+          '<p class="description">' + snippet + '</p>' +
+          '</li>';
 
-    header.css({
-      'height': '250px',
-      'padding-top': '1rem'
+
+        const mobile = window.matchMedia('(max-width: 600px)');
+        const tablet = window.matchMedia('(min-width: 601px) and (max-width: 1239px)')
+        const desktop = window.matchMedia('(min-width: 1240px)');
+
+        if (mobile.matches) {
+          $('.main-header').css('height', '250px');
+          $('.logo').css('height', '120px');
+        }
+
+        if (tablet.matches) {
+          $('.main-header').css('height', '165px');
+          $('.logo').css({
+            'height': '100px'
+          });
+          $('.select-wrapper').css({
+            'text-align': 'left',
+            'padding-left': '2rem'
+          });
+        }
+
+        if (desktop.matches) {
+          $('.main-header').css('height', '165px')
+          $('.logo').css('height', '100px');
+          $('footer').css({
+            'text-align': 'left',
+            'padding-left': '1rem'
+          });
+        }
+        output.append(results);
+      }
     });
-    logo.css('height', '150px');
-
-    output.append(results);
-    }
-  });
 }
 
 select.on('change', getData);
